bbe1f073436f00920b9737eea3f9e1964c6350db
motein <motein@qq.com>
2024-01-30 16:28:43 +0800

    Add Ch3

---
+++ HowLinuxWorks/Ch3/add.asm
@@ -0,0 +1,5 @@
+load m100 r0    // load the value in m100(variable 'a') to r0(register)
+load m200 r1    // load the value in m200(variable 'b') to r1(register)
+add r0 r1 r2    // Add r0 and r1, and store the result to r2(register)
+store r2 m300   // Store r2's value to m300(variable 'c')
+
